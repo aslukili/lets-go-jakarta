1be39567bdf0ca76795f8d61e3d7bbd2641f6f99
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-10-31 14:38:10 +0100

    update

--- src/main/java/me/aslukili/letsgojakarta/dao/BilliardsTableDao.java
+++ src/main/java/me/aslukili/letsgojakarta/dao/BilliardsTableDao.java
@@ -12,12 +12,22 @@ import org.hibernate.Transaction;
 public class BilliardsTableDao {
   public void saveBilliardsTable(BilliardsTable billiardsTable) {
     // TODO replace this with session factory !!! and use save instead of merge
+    Transaction transaction = null;
+
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
-    entityManager.getTransaction().begin();
-    entityManager.merge(billiardsTable);
-    entityManager.getTransaction().commit();
-    entityManager.close();
-    entityManagerFactory.close();
+
+
+    try  {
+      transaction = (Transaction) entityManager.getTransaction();
+      transaction.begin();
+      entityManager.merge(billiardsTable);
+      transaction.commit();
+    } catch (Exception e) {
+      if (transaction != null) {
+        transaction.rollback();
+      }
+      e.printStackTrace();
+    }
   }
 }
---
+++ src/main/java/me/aslukili/letsgojakarta/hibernate/HibernateFactory.java
@@ -0,0 +1,15 @@
+//package me.aslukili.letsgojakarta.hibernate;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//
+//public class HibernateFactory {
+//  public SessionFactory getSession(){
+//    Configuration configuration = new Configuration().configure();
+//    return configuration.buildSessionFactory();
+//  }
+//
+//  public static HibernateFactory getInstance(){
+//    return new HibernateFactory();
+//  }
+//}
